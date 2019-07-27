2c0d35b1e8846c475e89c8a71cabb090c9bf7eb8
williamamorim <william@os3ti.com>
2019-07-27 13:43:21 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import momemt from 'moment';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.png';
+
+import { Container, Form } from './styles';
+
+import CompareList from '../../components/CompareList';
+
+export default class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+  };
+
+  handleAddRespository = async (e) => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    try {
+      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+
+      repository.lastCommit = momemt(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, repository],
+        repositoryError: false,
+      });
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="github Compare" />
+
+        <Form withError={this.state.repositoryError} onSubmit={this.handleAddRespository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={this.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">
+            {this.state.loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}
+          </button>
+        </Form>
+
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
+}
---
+++ src/styles/global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+import 'font-awesome/css/font-awesome.css';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: #9b65e6;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    font-family: sans-serif
+  }
+`;
+
+export default GlobalStyle;
